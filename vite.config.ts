45ceb43362246916dbc5e5e63038a29cacbcddf2
boytrend0108 <boytrend@gmail.com>
2024-03-03 14:06:14 +0200

    acc deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Set base path to './' for relative paths
+  base: '/react_royal_house/', // Set base path to './' for relative paths
   plugins: [react()],
 })
